e8eb5c0d483260544ee05bd342342743125cf8c1
hildevops9 <muhammadhilmya.devops@gmail.com>
2026-06-13 00:19:19 +0700

    edit konten 5

--- src/App.jsx
+++ src/App.jsx
@@ -280,11 +280,13 @@ export default function App() {
       document.body.style.position = 'fixed'
       document.body.style.top = `-${scrollY}px`
       document.body.style.width = '100%'
+      document.body.style.overflow = 'hidden'
     } else {
       const scrollY = document.body.style.top
       document.body.style.position = ''
       document.body.style.top = ''
       document.body.style.width = ''
+      document.body.style.overflow = ''
       window.scrollTo(0, parseInt(scrollY || '0') * -1)
     }
   }, [isSearchFocused])
